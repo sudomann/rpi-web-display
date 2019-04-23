d0b3b9f4295266771a2c5d24aa1e471ddc70516e
Willy Njundong <wfnjundong0@frostburg.edu>
2019-04-23 10:48:32 -0400

    removed unnecessary lines

--- application.py
+++ application.py
@@ -1,30 +1,9 @@
-"""
-Demo Flask application to test the operation of Flask with socket.io
-
-Aim is to create a webpage that is constantly updated with random numbers from a background python process.
-
-30th May 2014
-
-===================
-
-Updated 13th April 2018
-
-+ Upgraded code to Python 3
-+ Used Python3 SocketIO implementation
-+ Updated CDN Javascript and CSS sources
-
-"""
-
-
 # Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
 from time import sleep
 from threading import Thread, Event
-
-
-__author__ = 'slynn'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
